f038d6290b8b55b8cbbda13102b8b608d8167908
ianfhunter <ianfhunter@gmail.com>
2020-03-30 09:00:56 +0100

    fix

--- generate.py
+++ generate.py
@@ -1,20 +1,35 @@
-class Monster():
+import yaml
+
+class Monster:
     def __init__(self, name, stats):
         self.name = name
+        self.stats = stats
+
+    def show(self):
+        print(self.name)
+        for key, value in self.stats.items():
+            print(key, value)
         
-        
-def generate_monster():
+def generate_monster(tier=0):
     stats = {
-      "ATT": 10
-      "DEF": 10
-      "AGL": 10
-      "CON": 10
+      "ATK": 10,
+      "DEF": 10,
+      "AGL": 10,
+      "CON": 10,
       "MAG": 10
     }
+
+    if tier == 0:
+        with open("monster-base-1.yaml") as base:
+            b = yaml.load(base)
+            print(b)
+    else:
+        raise NotImplementedError
+
     return Monster("Default", stats)
     
-if __name__ == __main__:
-  print("Generating 5 monsters")
-  for x in range(5):
-      m = generate_monster()
-      m.print
+print("Generating 5 monsters")
+for x in range(5):
+    for t in range(3): # tier
+        m = generate_monster(tier=0)
+        m.show()
